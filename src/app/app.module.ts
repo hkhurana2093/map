351cd47cc5a0720a3b0fb1b23ca8c34f7b4e2a59
hkhurana2093 <hkhurana.2093@gmail.com>
2019-05-10 14:12:00 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,39 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MapComponent } from './main/map/map.component';
+import { AgmCoreModule } from '@agm/core';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import {Routes,RouterModule} from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+const appRoutes:Routes = [
+  {
+      path: '',redirectTo: '/graph',pathMatch: 'full'},
+      { path: 'map',component: MapComponent,data: { title: 'map' } }
+      
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MapComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ModalModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC0McczTB0q4J-6XWlu9FAjncjXyVO8i5k'
+    })
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+  
+ }
