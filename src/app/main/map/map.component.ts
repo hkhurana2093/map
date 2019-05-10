d6c87846fea12db7731947485a3ae73f31abb03a
hkhurana2093 <hkhurana.2093@gmail.com>
2019-05-10 14:33:29 +0530

    3 files modified, 2 added

--- src/app/main/map/map.component.ts
+++ src/app/main/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
+
 
 @Component({
   selector: 'app-map',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+ title: string = 'My first AGM project';
+  lat: number = 30.733315;
+  lng: number = 76.779419;
+  places:any[]=[]; 
+  iconUrl:any;
+  googleicons:any;
+
+  constructor(private http: Http) { }
+
+ ngOnInit() {
+    this.iconUrl="assets/marker.png";
+    this.getJSON();
+  }
+getJSON() {
+this.http.get("location.json").subscribe(response => {
+          let temp = response.json();
+          this.places = temp.locations;
+
+          console.log("places", this.places);
+        })
   }
 
+  labelOptions = {
+    color: '#CC0000',
+    fontFamily: '',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    text: 'label 1',
+    }
+
+
+    private icon = {
+      url: 'assets/marker1.png', 
+      scaledSize: {
+        height: 50,
+        width: 30
+      }
+    };
 }
